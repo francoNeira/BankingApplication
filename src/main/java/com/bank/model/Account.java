b07dd90acc367e2a38793782f5189fb04f50f1cd
CristianAranguren <c.knoxville8@gmail.com>
2020-10-29 16:59:48 -0300

    modifico la clase Account y el BankingApplication para que utilicen la nueva clase Cash para la representacion del dinero
    Signed-off-by: CristianAranguren <c.knoxville8@gmail.com>

--- src/main/java/com/bank/model/Account.java
+++ src/main/java/com/bank/model/Account.java
@@ -1,15 +1,18 @@
 package com.bank.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
     private final Integer customerId;
     private final String customerName;
-    private Double balance;
     private Double previousTransaction;
+    private List<Cash> balances;
 
     public Account(Integer id, String name) {
         this.customerId = id;
         this.customerName = name;
-        this.balance = 0.0;
+        this.balances = new ArrayList<>();
         this.previousTransaction = null;
     }
 
@@ -19,8 +22,8 @@ public class Account {
     public String customerName() {
         return customerName;
     }
-    public Double balance() {
-        return balance;
+    public List<Cash> balances() {
+        return balances;
     }
     public Double previousTransaction() {
         return previousTransaction;
@@ -38,16 +41,35 @@ public class Account {
     /**
      * Increases balance and updates previous transaction.
      */
-    public void deposit(Double amount) {
-        balance += amount;
-        previousTransaction = amount;
+    public void deposit(Cash cash) {
+    	if(balancesInclude(cash)) {
+    		Cash balance = balances.stream().filter(current -> isRequiredCash(cash, current)).findFirst().get(); 
+    		balance.setValue(balance.getValue() + cash.getValue());
+    	}
+        balances.add(cash);
+        previousTransaction = cash.getValue();
     }
+    
+    public boolean isRequiredCash(Cash cash, Cash balance) {
+    	return balance.getCountry() == cash.getCountry() && balance.getClass() == cash.getClass();
+    }
+    
+    public boolean balancesInclude(Cash cash) {
+    	return balances.stream().anyMatch(balance -> isRequiredCash(cash, balance));
+    }
+    
+    public Cash searchRequiredCash(Cash cash) {
+    	return balances.stream().filter(current -> isRequiredCash(cash, current)).findFirst().get(); 
+    }
+    	
 
     /**
      * Decreases balance and updates previous transaction.
      */
-    public void withdraw(Double amount) {
-        balance -= amount;
-        previousTransaction = amount * -1;
+    public void withdraw(Cash cash) {
+    	Cash balance = balances.stream().filter(current -> isRequiredCash(cash, current)).findFirst().get(); 
+    	balance.setValue(balance.getValue() - cash.getValue());
+        previousTransaction = cash.getValue()*-1;
     }
 }
+
